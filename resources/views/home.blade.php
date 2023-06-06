4fee0c081b7b229e2551752ea7d1019ce5a6d2bf
andreaconv <andrea2convertini@gmail.com>
2023-06-06 16:28:48 +0200

    [edit] main wrapper dinamico

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,8 +10,10 @@
 
       <div class="main-wrapper">
 
-        {{-- <Cards :elenco="Comics"/> --}}
-        <div class="card"> <img src="https://imgs.search.brave.com/aUNyvZBXUulb963JH7KnQm9AMr8bcBoLsiHREOqayIU/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvOWZm/ZWYzMDMtMGZhYy00/OGRkLTg3ODctYzUy/NTk0MDk2ODAwXzEu/MTc1ZDk1Mjg2NzY0/OGEwOTczMTY2NGMy/MTE1NjNlYWIuanBl/Zz9vZG5XaWR0aD02/MTImb2RuSGVpZ2h0/PTYxMiZvZG5CZz1m/ZmZmZmY"> <span>testo</span> </div>
+        @foreach ($comics as $item)
+          <div class="card"> <img src="{{$item['thumb']}}"> <span>{{$item['title']}}</span> </div>
+        @endforeach
+
 
       </div>
 
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+  $comics = config('comics');
+  return view('home', compact('comics'));
 })->name('home');
 
 Route::get('/chi-siamo', function () {
-    return view('about');
+  return view('about');
 })->name('about');
