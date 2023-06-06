d071acc9f33aa4b43ab4e036861c7494c1b0c72d
andreaconv <andrea2convertini@gmail.com>
2023-06-06 16:17:14 +0200

    [add] main wrapper statico

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,24 @@
 
 @section('content')
 
+  <div class="myMain">
+
     <div class="container">
-        <h1>home</h1>
+
+      <span id="abs">CURRENT SERIES</span>
+
+      <div class="main-wrapper">
+
+        {{-- <Cards :elenco="Comics"/> --}}
+        <div class="card"> <img src="https://imgs.search.brave.com/aUNyvZBXUulb963JH7KnQm9AMr8bcBoLsiHREOqayIU/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvOWZm/ZWYzMDMtMGZhYy00/OGRkLTg3ODctYzUy/NTk0MDk2ODAwXzEu/MTc1ZDk1Mjg2NzY0/OGEwOTczMTY2NGMy/MTE1NjNlYWIuanBl/Zz9vZG5XaWR0aD02/MTImb2RuSGVpZ2h0/PTYxMiZvZG5CZz1m/ZmZmZmY"> <span>testo</span> </div>
+
+      </div>
+
+      <div class="btn">LOAD MORE</div>
+
     </div>
+
+  </div>
 
 @endsection
 
